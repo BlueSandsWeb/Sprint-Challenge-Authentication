714a3fad7dcc622a020813d14538faf0315cab5d
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-04-05 10:04:38 -0700

    added tokens and the basic implementation of login and register routes

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,7 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const db = require('../database/dbConfig.js');
+const jwt = require('jsonwebtoken');
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -8,13 +11,37 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
-function register(req, res) {
-  // implement user registration
+async function register(req, res) {
+  try {
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 12);
+    user.password = hash;
+    const [id] = await db('users').insert(user);
+    const newUser = await db('users').where({ id }).first();
+    const token = generateToken(newUser);
+    res.status(201).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: `error: ${error}` })
+  }
+
 }
 
-function login(req, res) {
-  // implement user login
+async function login(req, res) {
+  try {
+    let { username, password } = req.body;
+    const user = await db('users').where({ username }).first();
+    if (user && bcrypt.compareSync(password, user.password)) {
+      const token = generateToken(user);
+
+      res.status(200).json({ message: `welcome ${user.username}`, token });
+    } else {
+      res.status(401).json({ message: "Invalid Credentials" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error.  If this continues, please contact sysadmin" });
+  }
 }
+
 
 function getJokes(req, res) {
   const requestOptions = {
@@ -29,4 +56,16 @@ function getJokes(req, res) {
     .catch(err => {
       res.status(500).json({ message: 'Error Fetching Jokes', error: err });
     });
+}
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  };
+  const secret = process.env.JWT_SECRET;
+  const options = {
+    expiresIn: '1d',
+  }
+  return jwt.sign(payload, secret, options);
 }
