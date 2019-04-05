09453f9eafa2e98611dd1bff9dda5992048ab17e
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-04-05 10:11:51 -0700

    added check for username and password fields both being filled in for login and register routes

--- config/routes.js
+++ config/routes.js
@@ -14,12 +14,16 @@ module.exports = server => {
 async function register(req, res) {
   try {
     let user = req.body;
-    const hash = bcrypt.hashSync(user.password, 12);
-    user.password = hash;
-    const [id] = await db('users').insert(user);
-    const newUser = await db('users').where({ id }).first();
-    const token = generateToken(newUser);
-    res.status(201).json({ token });
+    if (user.username && user.password) {
+      const hash = bcrypt.hashSync(user.password, 12);
+      user.password = hash;
+      const [id] = await db('users').insert(user);
+      const newUser = await db('users').where({ id }).first();
+      const token = generateToken(newUser);
+      res.status(201).json({ token });
+    } else {
+      res.status(400).json({ message: "Please fill out all of the fields" });
+    }
   } catch (error) {
     res.status(500).json({ message: `error: ${error}` })
   }
@@ -29,13 +33,18 @@ async function register(req, res) {
 async function login(req, res) {
   try {
     let { username, password } = req.body;
-    const user = await db('users').where({ username }).first();
-    if (user && bcrypt.compareSync(password, user.password)) {
-      const token = generateToken(user);
+    if (username && password) {
+      const user = await db('users').where({ username }).first();
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
 
-      res.status(200).json({ message: `welcome ${user.username}`, token });
+        res.status(200).json({ message: `welcome ${user.username}`, token });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+
     } else {
-      res.status(401).json({ message: "Invalid Credentials" });
+      res.status(400).json({ message: "Please fill in all fields" });
     }
   } catch (error) {
     res.status(500).json({ message: "Internal Server Error.  If this continues, please contact sysadmin" });
